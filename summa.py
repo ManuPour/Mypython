91162087817322ae9c7b55b54efcbc6103f098fc
ManuPour <95218786+ManuPour@users.noreply.github.com>
2023-11-09 12:30:02 +0100

    Add files via upload

--- summa.py
+++ summa.py
@@ -1,12 +1,22 @@
-list =[4, 12, 7, 2, 3, 6 ,1 , 8, 5, 13, 16, 19]
+lista = [4, 7, 2, 3, 6, 1, 8, 5, 13, 16, 19]
 target = 12
 
-def summa12(list, target):
-    found = False
-    for i in range(len(list)-1):
-      for j in range(i+1, len(list)):
-          if list[i]+ list[j] == target:
-              print(list[i], list[j])
-              found = True
-    return found
-print(summa12(list, target))
+def summa12(lista, target):
+    resultat_lista = []
+    antalsokningar = 0
+
+    for i in range(len(lista)-1):
+        antalsokningar += 1
+        for j in range(i+1, len(lista)):
+
+            if lista[i] + lista[j] == target:
+                resultat_lista = lista[i], lista[j]
+                print("Tal i listan som är summan eller lika med target:", resultat_lista)
+                return 'Antal sökningar är:', antalsokningar
+            elif target == lista[i]:
+                resultat_lista = lista[i]
+                print("Tal i listan som är summan eller lika med target:", resultat_lista)
+                return 'Antal sökningar är:', antalsokningar
+    return
+
+print(summa12(lista, target))
